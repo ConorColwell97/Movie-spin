11255f5a8bf460dfaa470a1824c8886395afe919
ConorColwell97 <conormcolwell@gmail.com>
2025-09-03 10:46:03 +0100

    Added log in/sing up form

---
+++ src/pages/Home.jsx
@@ -0,0 +1,100 @@
+import './styles.css';
+import { useState } from 'react';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+
+const Home = () => {
+    const [home, setHome] = useState(true);
+    const [login, setLogin] = useState(false);
+    const [register, setRegister] = useState(false);
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const setToHome = () => {
+        setHome(true);
+        setLogin(false);
+        setRegister(false);
+    }
+
+    const setToLogin = () => {
+        setLogin(true);
+        setHome(false);
+    }
+
+    const setToRegister = () => {
+        setRegister(true);
+        setHome(false);
+    }
+
+    const createUser = async () => {
+        try {
+            const response = await axios.post(`http://localhost:8080/register`,{
+                username: username,
+                password: password
+            });
+
+            localStorage.setItem('jwt', response.data);
+
+            const decoded = jwtDecode(localStorage.getItem('jwt'));
+            console.log(`Your username is: ${decoded.sub}`);
+        } catch (error) {
+            console.log(error);
+        }
+
+        setUsername("");
+        setPassword("");
+    }
+
+    const userLogin = async () => {
+        try {
+            const response = await axios.post(`http://localhost:8080/userLogin`,{
+                username: username,
+                password: password
+            });
+
+            localStorage.setItem('jwt', response.data);
+
+            const decoded = jwtDecode(localStorage.getItem('jwt'));
+            console.log(`Your username is: ${decoded.sub}`);
+        } catch (error) {
+            console.log(error);
+        }
+
+        setUsername("");
+        setPassword("");
+    }
+
+    return (
+        <div className='login'>
+            {home && (
+                <>
+                    <button onClick={setToLogin}>Log in</button>
+                    <p>or</p>
+                    <button onClick={setToRegister}>Register</button>
+                </>
+            )}
+
+            {login && (
+                <>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username'/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password'/>
+                    <button onClick={userLogin}>Log in</button>
+                    <button onClick={setToHome}>Cancel</button>
+                </>
+            )}
+
+            {register && (
+                <>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username'/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password'/>
+                    <button onClick={createUser}>Register</button>
+                    <button onClick={setToHome}>Cancel</button>
+                </>
+            )}
+
+        </div>
+    );
+}
+
+export default Home;
