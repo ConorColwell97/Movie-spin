b686482548d305b67f553936e61a46af80fdbbfd
ConorColwell97 <conormcolwell@gmail.com>
2025-09-09 19:56:43 +0100

    Can now edit username and password

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import FindMovies from './pages/FindMovies';
 import Movies from './pages/Movies';
+import Profile from './pages/Profile';
 import './App.css'
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home/>}/>
         <Route path='/find' element={<FindMovies/>}/>
         <Route path='/movies' element={<Movies/>}/>
+        <Route path='/profile' element={<Profile/>}/>
       </Routes>
     </Router>
   )
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { useState } from "react";
+
+const Profile = () => {
+    const VITE_URL = import.meta.env.VITE_API_URL;
+    const [newName, setNewName] = useState("");
+    const [newPW, setNewPW] = useState("");
+    const [openName, setOpenName] = useState(false);
+    const [openPW, setOpenPW] = useState(false);
+
+    const updateName = async () => {
+        let response;
+
+        try {
+            response = await axios.patch(`${VITE_URL}/updatename/${encodeURIComponent(localStorage.getItem('user'))}`,
+                { newUsername: newName }, { withCredentials: true });
+
+            localStorage.setItem('user', newName);
+            setNewName("");
+            setOpenName(false);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const updatePassword = async () => {
+        let response;
+
+        try {
+            response = await axios.patch(`${VITE_URL}/updatepw/${encodeURIComponent(localStorage.getItem('user'))}`,
+                { newPassword: newPW }, { withCredentials: true });
+
+            setNewPW("");
+            setOpenPW(false);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div>
+            {openName ? (
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <input
+                        type="text"
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                        placeholder="Enter new username"
+                    />
+                    <button onClick={updateName} disabled={newName.length === 0}>Submit</button>
+                </div>
+
+            ) : (
+                <button onClick={() => setOpenName(true)}>Change Username</button>
+            )}
+
+            {openPW ? (
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <input
+                        type="password"
+                        value={newPW}
+                        onChange={(e) => setNewPW(e.target.value)}
+                        placeholder="Enter new password"
+                    />
+                    <button onClick={updatePassword} disabled={newPW.length < 8}>Submit</button>
+                </div>
+
+            ) : (
+                <button onClick={() => setOpenPW(true)}>Change Password</button>
+            )}
+        </div>
+    );
+}
+
+export default Profile;
