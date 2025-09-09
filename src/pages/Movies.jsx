22894ee3cd7b832288334bd235a260264854af05
ConorColwell97 <conormcolwell@gmail.com>
2025-09-09 16:08:35 +0100

    Delete movies function added

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Checkbox from '../components/Checkbox';
 import './styles.css';
 
 const Movies = () => {
     const [data, setData] = useState(null);
+    const [moviesToRemove, setMoviesToRemove] = useState([]);
+    const disabled = (moviesToRemove.length === 0);
     const VITE_URL = import.meta.env.VITE_API_URL;
 
     const getMovies = async () => {
@@ -11,12 +14,36 @@ const Movies = () => {
 
         try {
             response = await axios.get(`${VITE_URL}/getmovies/${encodeURIComponent(localStorage.getItem('user'))
-            }`,{ withCredentials: true });
+                }`, { withCredentials: true });
 
-            console.log(response.data);
             setData(response.data);
         } catch (err) {
             console.log(err);
+        }
+    }
+
+    const deleteMovies = async () => {
+        let response;
+
+        try {
+            response = await axios.patch(`${VITE_URL}/deletemovies/${encodeURIComponent(localStorage.getItem('user'))}`,
+            moviesToRemove, { withCredentials: true });
+
+            getMovies();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const addItem = (setArr, item, checked) => {
+        if (checked) {
+            setArr(prev => {
+                return [...prev, item];
+            });
+        } else {
+            setArr(prev => {
+                return prev.filter(element => element != item);
+            });
         }
     }
 
@@ -26,15 +53,26 @@ const Movies = () => {
 
     return (
         <div className='main'>
-            {data && (
-                data.movies.map((movie, index) => (
-                    <div className='container' key={index}>
-                        <p>{movie.title}</p>
-                        <p>{movie.genres}</p>
-                        <p>{movie.releaseDate}</p>
-                        <p>{movie.overview}</p>
-                    </div>
-                ))
+            {data !== null ? (
+                <>
+                    {data.movies.map((movie, index) => (
+                        <div className='container' key={index}>
+                            <p>{movie.title}</p>
+                            <p>{movie.genres}</p>
+                            <p>{movie.releaseDate}</p>
+                            <p>{movie.overview}</p>
+                            <label>
+                                Mark as watched:
+                                <Checkbox visible={true} action={addItem} item={movie} setArr={setMoviesToRemove} />
+                            </label>
+
+                        </div>
+                    ))}
+                    <button disabled={disabled} onClick={deleteMovies}>Delete all marked as watched?</button>
+                </>
+
+            ) : (
+                <p>You do not have any movies in your collection</p>
             )}
         </div>
     );
